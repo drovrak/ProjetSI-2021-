7ecec29e29089ee3719cd0a8a08991fde78a823d
drovrak <42212416+drovrak@users.noreply.github.com>
2021-03-31 14:37:51 +0200

    update du prog et réglage pour les capteurs

--- alarme/programmeAlarme_V2.c
+++ alarme/programmeAlarme_V2.c
@@ -1,19 +1,15 @@
+#include <Ultrasonic.h>
 //helloThere
 int floatSensor = 2;
 int bzr = 3;
-int _trig_ = 4; //émission
-int _echo_ = 5; //réception
-long lecture_echo;
-long cm;
+Ultrasonic ultrasonic(7);
+long RangeInCentimeters;
 int btnState = 1; //btnState = 1 par default
 
 void setup()
 {
   pinMode(floatSensor, INPUT);  //position reception du bouton
   pinMode(bzr, OUTPUT);         //position sortie du buzzer
-  pinMode(_trig_, OUTPUT);      //position sortie du capteur (emission)
-  digitalWrite(_trig_,LOW);     //position éteint au demarrage du capteur de song
-  pinMode(_echo_,INPUT);        //position entrée du capteur (reception)
   Serial.begin(9600);           //demarrage de la com serie
 }
 
@@ -33,28 +29,21 @@ void loop(){
   if (btnState == HIGH){ //si le switch = true alors on buzze
     tone(bzr, 100);
     delay(200);
+    Serial.print("up");
   }
-  else{noTone(bzr);}
+  else{noTone(bzr); Serial.print("down");}
 //programme de détection des rebords
-    //émission signal
-    digitalWrite(_trig_, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(_trig_, LOW);
 
     //lecture de l'echo
-    lecture_echo = pulseIn(_echo_,HIGH);
+   
+    RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
+    Serial.print(RangeInCentimeters);//0~400cm
+    Serial.println(" cm");
+    delay(200);
 
-    //calcule de la distance
-    cm = lecture_echo /3333;
-
-    //print de la distance dans le terminale serie
-    //(à supprimer dans l'avenir)
-    Serial.print("Distance cm :");
-    Serial.println(cm);
-    delay(1000);
-
-    if(cm < 5){//declanchement du buzzer selon la distance robot->bord
-      tone(bzr, 100);
+    if(RangeInCentimeters <= 5){//declanchement du buzzer selon la distance robot->bord
+      tone(bzr, 10);
       delay(200);}
     else{noTone(bzr);}
 }
+//Les serialprint seront à suprimmer à la fin !
